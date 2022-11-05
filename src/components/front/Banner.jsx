714fa0c1dd7b8867e57fa7e7a62f1f6eb2a8b6b9
alizab5000005 <alizab5000005@outlook.com>
2022-11-05 12:59:29 +0500

    another commit

--- src/components/front/Banner.jsx
+++ src/components/front/Banner.jsx
@@ -11,35 +11,26 @@ const Banner = () => {
         </ol>
         <div className="carousel-inner">
             <div className="carousel-item active">
-                <img className="img-fluid" src="img/carousel-1.jpg" alt="Image"/>
+                <img className="img-fluid" src="banner.png" style={{width:'100%'}} alt="Image"/>
                 <div className="carousel-caption d-flex align-items-center justify-content-center">
                     <div className="p-5" style={{width: '100%', maxWidth: '900px'}}>
-                        <h5 className="text-primary text-uppercase mb-md-3">Cleaning Services</h5>
-                        <h1 className="display-3 text-white mb-md-4">Best Quality Solution In Cleaning</h1>
+                        <h5 className="text-primary text-uppercase mb-md-3">Construction Services</h5>
+                        <h1 className="display-3 text-white mb-md-4">Best Quality Solution In Construction</h1>
                         <a href="" className="btn btn-primary">Get A Quote</a>
                     </div>
                 </div>
             </div>
             <div className="carousel-item">
-                <img className="img-fluid" src="img/carousel-2.jpg" alt="Image"/>
+                <img className="img-fluid" src="banner2.jpg" style={{width:'100%'}} alt="Image"/>
                 <div className="carousel-caption d-flex align-items-center justify-content-center">
                     <div className="p-5" style={{width: '100%', maxWidth: '900px'}}>
-                        <h5 className="text-primary text-uppercase mb-md-3">Cleaning Services</h5>
-                        <h1 className="display-3 text-white mb-md-4">Highly Professional Cleaning Services</h1>
+                        <h5 className="text-primary text-uppercase mb-md-3">Maintainanace Services</h5>
+                        <h1 className="display-3 text-white mb-md-4">Highly Professional Maintainanace Services</h1>
                         <a href="" className="btn btn-primary">Get A Quote</a>
                     </div>
                 </div>
             </div>
-            <div className="carousel-item">
-                <img className="img-fluid" src="img/carousel-3.jpg" alt="Image"/>
-                <div className="carousel-caption d-flex align-items-center justify-content-center">
-                    <div className="p-5" style={{width: '100%', maxWidth: '900px'}}>
-                        <h5 className="text-primary text-uppercase mb-md-3">Cleaning Services</h5>
-                        <h1 className="display-3 text-white mb-md-4">Experienced & Expert Cleaners</h1>
-                        <a href="" className="btn btn-primary">Get A Quote</a>
-                    </div>
-                </div>
-            </div>
+            
         </div>
     </div>
 </div>
